6d683ef05fdc8d39e3e31ec89c60cab15ec9ec73
Frank <frankdavid93om@yahoo.com>
2023-09-19 20:18:12 -0400

    working on one to many relations to show all info for a specific patlog log number

--- Migrations/20230920001652_removed Icollection adn setter.Designer.cs
+++ Migrations/20230920001652_removed Icollection adn setter.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SharkValleyServer.Data;
 
@@ -11,9 +12,11 @@ using SharkValleyServer.Data;
 namespace SharkValleyServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230920001652_removed Icollection adn setter")]
+    partial class removedIcollectionadnsetter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
