b2376ead9003f30afb0bdef1274f04cd816f4171
Frank <frankdavid93om@yahoo.com>
2023-09-22 23:49:14 -0400

    added ended patrol timer

--- Controllers/UserTimerController.cs
+++ Controllers/UserTimerController.cs
@@ -48,10 +48,9 @@ namespace SharkValleyServer.Controllers
         }
 
 
-        // Adding post method to api
+        // start patrol timer post method api
         [HttpPost("startTime")]
-
-        public async Task<ActionResult> Post([FromBody] UserTimerDto dto)
+        public async Task<ActionResult> PostStartPatrolTimer([FromBody] UserTimerDto dto)
         {
 
             // Get the api key from headers when user send post request
@@ -80,7 +79,6 @@ namespace SharkValleyServer.Controllers
 
             // check if patrolLogId and email exist in the table rturn bad repsonse else add start timer
             var logExist = dbContext.UserTimers.Where(ut => ut.Email == dto.Email & ut.PatrolLogId == patrolLog.Id).FirstOrDefault();
-
 
 
             Console.WriteLine(logExist.Email);
@@ -115,12 +113,77 @@ namespace SharkValleyServer.Controllers
                 dbContext.SaveChanges();
 
             }
-
-
-
             return Ok("Timer Started");
-
         }
+
+
+        // end patrol timer post method api
+        [HttpPost("endTimer")]
+        public async Task<ActionResult> PostEndPatrolTimer([FromBody] UserTimerDto dto)
+        {
+
+            // Get the api key from headers when user send post request
+            if (!Auth.IsValidAPIKey(Request))
+                return Unauthorized();
+
+
+            string? userId = Auth.getUserId(Request);
+            if (userId == null)
+                return Unauthorized(Request);
+
+            
+            // verify users
+            IdentityUser?  user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+                return NotFound();
+
+
+
+            // get current patrolLogID from Settings
+            var currentPatrolLogId = await dbContext.Settings.FindAsync("PatrolNo");
+
+            // get current patrolLog for the specified patrolNo
+            var patrolLog = dbContext.PatrolLogs.Where(pl => pl.PatrolNo == currentPatrolLogId.Value.ToString()).FirstOrDefault();
+
+
+            // check if patrolLogId and email exist in the table rturn bad repsonse else add start timer
+            var logExist = dbContext.UserTimers.Where(ut => ut.Email == dto.Email & ut.PatrolLogId == patrolLog.Id).FirstOrDefault();
+
+
+            if(logExist == null)
+            {
+                Console.WriteLine("it does not exist log");
+
+
+            }else{
+                Console.WriteLine("it exists log");
+            }
+
+
+
+            if(logExist.hasEndedPatrol == true)
+            {
+                //handle return if user hasStartedPatrol
+
+                return Ok("Sorry You Already Ended Patrol");
+            }
+            else
+            {
+            
+                // getting body data
+                logExist.EndedPatrolTime = dto.EndedPatrolTime;
+
+                logExist.hasEndedPatrol = true;
+
+                // save startedPatrolTime to db
+                dbContext.SaveChanges();
+
+            }
+            return Ok("Patrol Timer Ended");
+        }
+
+
+
 
     }
 }
